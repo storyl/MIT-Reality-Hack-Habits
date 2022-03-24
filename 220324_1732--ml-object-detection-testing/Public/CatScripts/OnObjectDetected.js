6718c30bf206d54130ced4e9191e132d393a8494
Andy Fillebrown <andy@docEdub.com>
2022-03-24 19:21:42 -0400

    Get good and bad cat animations working for cat and dog object detections

--- 220324_1732--ml-object-detection-testing/Public/CatScripts/OnObjectDetected.js
+++ 220324_1732--ml-object-detection-testing/Public/CatScripts/OnObjectDetected.js
@@ -1,31 +1,46 @@
 // -----JS CODE-----
 //@input Component.ScriptComponent mlOutputDecoderScript
-//@input SceneObject sceneObject = null
+//@input SceneObject goodAnimation = null
+//@input SceneObject badAnimation = null
 
+const CatClass = 3;
+const DogClass = 5;
 
 function onDetectionsUpdated(results) {
-    var isObjectDetected = false;    
+    var isObjectDetected = false;  
+    var detectedObjectClass = 0;
     
-    print("onDetectionsUpdated called")
+//    print("onDetectionsUpdated called")
     var resultsKeys = Object.keys(results)
-    print("resultsKeys count = " + resultsKeys.length)
-    print("resultsKeys = " + resultsKeys)
+//    print("resultsKeys count = " + resultsKeys.length)
+//    print("resultsKeys = " + resultsKeys)
     
     for (var i = 0; i < resultsKeys.length; i += 1) {
-        var result = results[i.toString()]
+        var result = results[i]
         if (result) {
             print(i + ": box = " + result.box)
             print("   score = " + result.score)
             print("   class = " + result.class)
             isObjectDetected = true;
+            detectedObjectClass = result.class
         }
         else {
-            print(i + ": undefined")
+//            print(i + ": undefined")
+        }
+    }
+
+    if (script.goodAnimation) {
+        script.goodAnimation.enabled = false
+        if (isObjectDetected && detectedObjectClass === CatClass) {
+            script.goodAnimation.enabled = true
         }
     }
     
-    if (script.sceneObject) {
-        script.sceneObject.enabled = isObjectDetected
+    if (script.badAnimation) {
+        script.badAnimation.enabled = false
+        if (isObjectDetected && detectedObjectClass === DogClass) {
+            script.badAnimation.enabled = true
+        }
     }
 }
 
